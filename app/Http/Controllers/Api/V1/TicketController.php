f711a932fb2cca4bbb682dd3f43290d8e8e8217a
Guilherme Souza <ghostgui60@gmail.com>
2025-03-05 21:11:50 -0300

    remove: Remove unnecessary methods

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -19,14 +19,6 @@ class TicketController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTicketRequest $request)
@@ -41,15 +33,6 @@ class TicketController extends Controller
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Ticket $ticket)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      */
