e6554d309dee086f692843030723719530f67496
Guilherme Souza <ghostgui60@gmail.com>
2025-03-20 21:00:30 -0300

    feat: add the auth flow

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('tickets', TicketController::class);
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    // Auth Routes (No Middleware)
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
-
+    // Protected Routes (Sanctum)
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('tickets', TicketController::class);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
+});
