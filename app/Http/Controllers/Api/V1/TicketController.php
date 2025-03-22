82547a2316726fb32a326f87890fa1ba28babdc1
Guilherme Souza <ghostgui60@gmail.com>
2025-03-22 17:21:29 -0300

    feat: Paginate the results of tickets

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
+use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
 
 class TicketController extends Controller
@@ -14,8 +15,7 @@ class TicketController extends Controller
      */
     public function index()
     {
-        $tickets = Ticket::all();
-        return response()->json($tickets);
+        return TicketResource::collection(Ticket::paginate());
     }
 
     /**
@@ -31,7 +31,7 @@ class TicketController extends Controller
      */
     public function show(Ticket $ticket)
     {
-        //
+        return new TicketResource($ticket);
     }
     /**
      * Update the specified resource in storage.
