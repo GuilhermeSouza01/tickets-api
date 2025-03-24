25b6a374787efb145ab6c974fc3efb6325458f61
Guilherme Souza <ghostgui60@gmail.com>
2025-03-24 15:54:14 -0300

    feat: add users route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ Route::prefix('v1')->group(function () {
     // Protected Routes (Sanctum)
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('tickets', TicketController::class);
+        Route::apiResource('users', UsersController::class);
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/user', function (Request $request) {
             return $request->user();
