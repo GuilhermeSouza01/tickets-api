02bccdb31ba7c729e8f91af4d97bac8287f9efab
Guilherme Souza <ghostgui60@gmail.com>
2025-03-31 22:28:03 -0300

    feat: add method to authorize user to make operations with yourself tickets

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponses;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    use ApiResponses;
+
+    use ApiResponses, AuthorizesRequests;
+    protected $policyClass;
     public function include(string $relationship):bool
     {
         $param = request()->get('include');
@@ -19,5 +23,14 @@ class ApiController extends Controller
         $includeValues = explode(',', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+    /**
+     * @throws AuthorizationException
+     */
+    public function isAble($ability, $targetModel)
+    {
+         return $this->authorize($ability, [$targetModel, $this->policyClass]);
+
     }
 }
