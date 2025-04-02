505b704177529833aba3accb5ad5f22dd3f70415
Guilherme Souza <ghostgui60@gmail.com>
2025-04-01 21:21:36 -0300

    feat: insert the abilities on user login method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginUserRequest;
 use App\Models\User;
+use App\Permissions\V1\Abilities;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,7 +26,10 @@ class AuthController extends Controller
         return $this->ok(
             'Authenticated',
             [
-                'token' => $user->createToken('Api  Token for' . $user->email, ["*"], now()->addMonth())->plainTextToken,
+                'token' => $user->createToken(
+                    'Api  Token for' . $user->email,
+                    Abilities::getAbilities($user),
+                    now()->addMonth())->plainTextToken,
             ]
         );
     }
