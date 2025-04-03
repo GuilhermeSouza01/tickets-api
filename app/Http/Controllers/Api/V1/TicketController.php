903955dfa797cef10d2e874c2735c9d30113385c
Guilherme Souza <ghostgui60@gmail.com>
2025-04-02 21:22:53 -0300

    fix: change the way of store a ticket

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -31,17 +31,11 @@ class TicketController extends ApiController
     public function store(StoreTicketRequest $request)
     {
         try {
-            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
-
-            $this->isAble('store', null);
-
-            //TODO create ticket
-        } catch (ModelNotFoundException $e) {
-            return $this->ok('User not found.', [
-                'error' => 'The provided user id does not exist.'
-            ]);
+            $this->isAble('store', Ticket::class);
+            return new TicketResource(Ticket::create($request->mappedAttributes()));
+        } catch (AuthorizationException $e) {
+            return $this->errorResponse('You are not authorized to perform this action.', 401);
         }
-        return new TicketResource($request->mappedAttributes());
     }
 
     /**
