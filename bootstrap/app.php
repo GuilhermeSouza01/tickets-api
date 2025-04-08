67d020c86213d1b6a23f5b1f47fec4fd3bb9e05b
Guilherme Souza <ghostgui60@gmail.com>
2025-04-08 18:43:18 -0300

    feat: create exception handler messages pattern

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Traits\ApiResponses;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +20,70 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e, $request) {
+            $responses = new class {
+                use ApiResponses;
+            };
+
+            if ($e instanceof ValidationException) {
+                $errors = [];
+                foreach ($e->errors() as $field => $messages) {
+                    $fieldName = str($field)->afterLast('.');
+                    foreach ($messages as $message) {
+                        $errors[] = [
+                            'status' => 422,
+                            'message' => $message,
+                            'source' => $fieldName
+                        ];
+                    }
+                }
+
+                return $responses->errorResponse($errors, 422);
+            }
+
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json([
+                    'errors' => [
+                        [
+                            'status' => 404,
+                            'message' => 'The resource cannot be found.',
+                            'source' => $e->getModel()
+                        ]
+                    ]
+                ], 404);
+            }
+            if($e instanceof NotFoundHttpException){
+                return response()->json([
+                   'errors' => [
+                       [
+                           'status' => 404,
+                           'message' => 'The resource cannot be found.',
+                           'type'=> 'NotFoundHttpException'
+                       ]
+                   ]
+                ]);
+            }
+
+            if ($e instanceof AuthenticationException) {
+                return response()->json([
+                    'errors' => [
+                        [
+                            'status' => 401,
+                            'message' => 'Unauthenticated',
+                            'source' =>  'Line: ' . $e->getLine() . ': ' . $e->getFile()
+                        ]
+                    ]
+                ], 401);
+            }
+
+            return response()->json([
+                'errors' => [
+                    [
+                        'status' => 500,
+                        'message' => 'An unexpected error occurred.',
+                        'type' => class_basename($e)
+                    ]
+                ]
+            ], 500);
+        });
     })->create();
